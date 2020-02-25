ab3402d21ff849f8fb61c5bc11f3d96392fdfb47
Thordur Hermannsson <doddi@kott.is>
2020-02-25 23:08:08 +0000

    add gitignore and pass env variables as arguments to nix instead of copying the env file

--- default.nix
+++ default.nix
@@ -1,4 +1,8 @@
-{ yarn2nixPath ? /Users/Donna/Code/K0TT/github/yarn2nix }:
+{ yarn2nixPath ? /Users/Donna/Code/K0TT/github/yarn2nix 
+, port ? "8080"
+, apiURL ? ""
+, apiKey ? ""
+}:
   let
     pkgs = import ./packages.nix {};
     spagoPkgs = import ./spago-packages.nix { inherit pkgs; };
@@ -12,6 +16,14 @@
      removeHashBang = drv: drv.overrideAttrs (oldAttrs: {
        buildCommand = builtins.replaceStrings ["#!/usr/bin/env"] [""] oldAttrs.buildCommand;
      });
+
+    gitignoreSrc = pkgs.fetchFromGitHub { 
+      owner = "hercules-ci";
+      repo = "gitignore.nix";
+      rev = "7415c4feb127845553943a3856cbc5cb967ee5e0";
+      sha256 = "sha256:1zd1ylgkndbb5szji32ivfhwh04mr1sbgrnvbrqpmfb67g2g3r9i";
+    };
+    inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
     
     yarn2nix = import yarn2nixPath { inherit pkgs; };
     npm = yarn2nix.mkYarnPackage {
@@ -25,7 +37,7 @@
   in
     pkgs.stdenv.mkDerivation {
       name = "donnabot.dev-frontend";
-      src = ./.;
+      src = gitignoreSource ./.;
       buildInputs = [ 
         easy-ps.purs easy-ps.spago npm pkgs.nodejs-12_x
       ];
@@ -37,7 +49,7 @@
             (x: ''"${x.outPath}/src/**/*.purs"'')
             (builtins.attrValues spagoPkgs.inputs))}
         mv output $out
-        cp -r ${npm}/libexec/blog/node_modules $out
+        cp -r ${npm}/libexec/donnabot.dev/node_modules $out
       '';
 
       installPhase = ''
@@ -45,17 +57,19 @@
         mkdir -p $out/static
 
         # Copy static files
-        cp -r $src/static/img $out/static
+        cp -r $src/static/img   $out/static
         cp -r $src/static/style $out/static
         cp -r $src/static/views $out/static
 
-        cp $src/.env  $out/.env
+        echo PORTNR=${port} >> $out/.env
+        echo API_URL=${apiURL} >> $out/.env
+        echo API_KEY=${apiKey} >> $out/.env
 
         # Make bundle
         cd $out && spago bundle-app --no-install --no-build --to $out/static/build/index.js
 
         # Browserify bundle
-        cd $out && ${npm}/libexec/blog/node_modules/parcel/bin/cli.js build $out/static/build/index.js -d $out/static/dist
+        cd $out && ${npm}/libexec/donnabot.dev/node_modules/parcel/bin/cli.js build $out/static/build/index.js -d $out/static/dist
 
         # Bundle Server
         cd $out && spago bundle-app --main Server --no-install --no-build --to $out/output/donnabot/server.js
