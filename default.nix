806564d9b878995b238f61b425cbbe4bfbbe704d
Thordur Hermannsson <doddi@kott.is>
2020-02-25 23:17:22 +0000

    pin easy-purescript-nix

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@
     easy-ps = import (pkgs.fetchFromGitHub {
       owner = "justinwoo";
       repo = "easy-purescript-nix";
-      rev = "refs/heads/master";
+      rev = "340e82b6ecaccc4059740e69f8ec18546b527481";
       sha256 = "1q2ciwd3193kig1paidzrgxl60y4rb39bsi97lk7m6ff8mis6z6i";
     }) { inherit pkgs; };
 
