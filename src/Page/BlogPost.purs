ea8ebcd8cd470721526b4c32ed243d7ff34947d2
Thordur Hermannsson <doddi@kott.is>
2020-05-09 00:01:16 +0000

    add dynamic meta info

--- src/Page/BlogPost.purs
+++ src/Page/BlogPost.purs
@@ -12,11 +12,18 @@ import Foreign.LazyLoad             as LZ
 import Halogen                      as H
 import Halogen.HTML                 as HH
 import Slug                         as Slug
+import Web.HTML                     (window)
+import Web.DOM.Document             as DOC
+import Web.DOM.Element              as DOM
+import Web.DOM.HTMLCollection       as HTMLCollection
+import Web.HTML.HTMLDocument        as HTMLDoc
+import Web.HTML.Window              as Window
 
 import Component.HTML.BlogPost      (renderBlogPost)
 import Component.HTML.Header        (header)
 import Component.HTML.Utils         (css, safeHref)
 import Data.BlogPost                (BlogPost(..))
+import Data.Image                   (Image(..))
 import Data.Route                   as R
 import Resource.BlogPost            (class ManageBlogPost
                                     ,getBlogPostBySlug)
@@ -72,6 +79,24 @@ component =
         H.liftEffect $ LZ.lazyLoad ".lazy"
         case blogPost of
           Just (BlogPost post) -> do
+            -- Update og meta info
+            -- TODO: Maybe put this into a util function?
+            document <- H.liftEffect $ window >>= Window.document
+            metas <- H.liftEffect $ DOC.getElementsByTagName "meta" $ HTMLDoc.toDocument document
+            titleMeta <- H.liftEffect $ HTMLCollection.namedItem "og:title" metas
+            imageMeta <- H.liftEffect $ HTMLCollection.namedItem "og:image" metas
+            case titleMeta of
+              Just tMeta ->
+                H.liftEffect $ DOM.setAttribute "content" post.title tMeta
+              Nothing -> pure unit
+            case imageMeta of
+              Just iMeta ->
+                case post.featuredImage of
+                  Just (Image img) -> case img.thumbnail of
+                    Just thumb -> H.liftEffect $ DOM.setAttribute "content" thumb iMeta
+                    Nothing -> H.liftEffect $ DOM.setAttribute "content" img.src iMeta
+                  Nothing -> pure unit
+              Nothing -> pure unit
             let label = "element-" <> (show $ unwrap post.id)
             H.getHTMLElementRef (H.RefLabel label) >>= case _ of
               Nothing -> pure unit
@@ -79,7 +104,6 @@ component =
                 case post.htmlContent of
                   Just html -> do
                     H.liftEffect $ setHTML el html
-                    -- Update og meta info
                   Nothing -> pure unit
           Nothing -> pure unit
       Nothing -> pure unit
